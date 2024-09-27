b66bc7f034cd0edb9fd5de3cfe43989d47c94412
meghaninave27 <meghaninave8@gmail.com>
2024-09-27 21:09:33 +0530

    Add files via upload

--- 43.c
+++ 43.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 #include<conio.h>
 int main()
-
 {
 	int i,j;
-	for(i=5;i>=1;i--)
+	for(i=1;i<=5;i++)
 	{
 		for(j=i;j<=5;j++)
 		{
-			printf("%d ",i);
+			if(j%2==0)
+			{
+				printf("0 ");
+			}
+			else
+			{
+				printf("1 ");
+			}
 		}
 		printf("\n");
 	}
