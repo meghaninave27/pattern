89a3da55a72b326caed421cb7feaba474a46fffe
meghaninave27 <meghaninave8@gmail.com>
2024-09-27 21:13:12 +0530

    Add files via upload

---
+++ 51.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,j;
+	char k='a';
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			printf("%c ",k);
+			k++;
+			
+			
+				
+		}
+		printf("\n");
+	}		
+		
+}
